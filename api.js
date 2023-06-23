1a561a3271da5e684e76141869e831f7aaedd4ad
nat-0813 <129187214+nat-0813@users.noreply.github.com>
2023-06-23 08:56:12 -0400

    api changes

--- api.js
+++ api.js
@@ -1,50 +1,96 @@
-const fs = require("fs");
-const express = require("express");
-const app = express();
-const axios = require('axios');
-const https = require("https");
-const bodyParser = require("body-parser");
-const port = 9000;
-app.listen(port, () => {
-    console.log(`App is running on port ${port}...`);
-});
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.post('/', async (req, res) => {
-    const education = req.body.education;
-    const options = {
-        method: 'GET',
-        url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI',
-        params: {
-          q: 'taylor swift',
-          pageNumber: '1',
-          pageSize: '10',
-          autoCorrect: 'true'
-        },
-        headers: {
-          'X-RapidAPI-Key': '6c097732b3msh0e449dff7fd3753p1dc597jsn4bdefd6b7870',
-          'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
-        }
-      };
-      
-      try {
-          const response = await axios.request(options);
-          console.log(response.data);
-      } catch (error) {
-          console.error(error);
-      }
+// const fs = require("fs");
+// const express = require("express");
+// const app = express();
+// const axios = require('axios');
+// // const https = require("https");
+// const bodyParser = require("body-parser");
+// const port = 9000;
+// app.listen(port, () => {
+//     console.log(`App is running on port ${port}...`);
+// });
+// app.set('view engine', 'js');
+// app.use(express.static('/'));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-    try {
-      const response = await axios.request(options);
-      const ImageSearchAPI = response.data.text;
-      res.status(200).render('home', { ImageSearchAPI }); // Update the render statement
-  } catch (error) {
-      console.error(error);
-      res.status(500).send('Error retrieving data from the API.');
-  }
-  });
-// get requests from html to server
-app.get('/edu', (req, res) => {
-    res.status(200).sendFile(__dirname + '/edu.html');
-});
+
+// app.post('/', async (req, res) => {
+//     const education = req.body.education;
+//     const options = {
+//       method: 'GET',
+//       url: 'https://google-search72.p.rapidapi.com/search',
+//       params: {
+//         q: 'word cup',
+//         gl: 'us',
+//         lr: 'lang_en',
+//         num: '10',
+//         start: '0'
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': '6c097732b3msh0e449dff7fd3753p1dc597jsn4bdefd6b7870',
+//         'X-RapidAPI-Host': 'google-search72.p.rapidapi.com'
+//       }
+//     };
+      
+//       try {
+//           const response = await axios.request(options);
+//           console.log(response.data);
+//       } catch (error) {
+//           console.error(error);
+//       }
+
+//     try {
+//       const response = await axios.request(options);
+//       const search = response.data.text;
+//       res.status(200).render('index', { search }); // Update the render statement
+//   } catch (error) {
+//       console.error(error);
+//       res.status(500).send('Error retrieving data from the API.');
+//   }
+//   });
+// // get requests from html to server
+// app.get('/', (req, res) => {
+//     res.status(200).sendFile(__dirname + '/api.html');
+    
+
+   
+
+// });
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '04a7b63742mshe31a0e0ebbb49f8p1cd7ebjsn812e3735989f',
+		'X-RapidAPI-Host': 'youtube-search-results.p.rapidapi.com'
+	}
+};
+
+
+
+const userInput = document.querySelector('input');
+const btn = document.querySelector('button');
+
+let params = ''
+const callParams = () => {
+    params = userInput.value
+    fetch(`https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${params}`, options)
+        .then(response => response.json())
+        .then(data => {
+            let output = '';
+            data.items.map(item => {
+                output += `
+                    <h1>${item.title}</h1>
+                    <li>
+                    <a href="${item.url}">${item.url}</a>
+                    </li>
+                    `;
+            })
+            document.querySelector('.result').innerHTML = output;
+        })
+        .catch(err => console.error(err));
+    userInput.value = '';
+}
+
+
+
+
+btn.addEventListener('click', callParams)
--- edu.js
+++ edu.js
@@ -1,59 +1,59 @@
-'use strict';
+// 'use strict';
 
-const searchForm = document.getElementById('searchForm');
-const searchInput = document.getElementById('searchInput');
-const dataContainer = document.getElementById('dataContainer');
+// const searchForm = document.getElementById('searchForm');
+// const searchInput = document.getElementById('searchInput');
+// const dataContainer = document.getElementById('dataContainer');
 
-searchForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const searchQuery = searchInput.value;
-  const apiUrl = `https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI`
+// searchForm.addEventListener('submit', async (e) => {
+//   e.preventDefault();
+//   const searchQuery = searchInput.value;
+//   const apiUrl = `https://google-search72.p.rapidapi.com/search`
 
-  try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        'X-RapidAPI-Key': '6c097732b3msh0e449dff7fd3753p1dc597jsn4bdefd6b7870',
-    'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
-      }
-    });
-    const data = await response.json();
-    populateData(data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-});
+//   try {
+//     const response = await fetch(apiUrl, {
+//       headers: {
+//         'X-RapidAPI-Key': '6c097732b3msh0e449dff7fd3753p1dc597jsn4bdefd6b7870',
+//     'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+//       }
+//     });
+//     const data = await response.json();
+//     populateData(data);
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//   }
+// });
 
-function populateData(data) {
-  dataContainer.innerHTML = '';
+// function populateData(data) {
+//   dataContainer.innerHTML = '';
 
-  if (data.length === 0) {
-    dataContainer.textContent = 'No results found.';
-    return;
-  }
+//   if (data.length === 0) {
+//     dataContainer.textContent = 'No results found.';
+//     return;
+//   }
 
-  data.forEach((item) => {
-    const div = document.createElement('div');
-    const editButton = document.createElement('button');
-    const deleteButton = document.createElement('button');
+//   data.forEach((item) => {
+//     const div = document.createElement('div');
+//     const editButton = document.createElement('button');
+//     const deleteButton = document.createElement('button');
 
-    div.textContent = `Name: ${item.name}, Description: ${item.description}`;
-    editButton.textContent = 'Edit';
-    deleteButton.textContent = 'Delete';
+//     div.textContent = `Name: ${item.name}, Description: ${item.description}`;
+//     editButton.textContent = 'Edit';
+//     deleteButton.textContent = 'Delete';
 
-    editButton.addEventListener('click', () => {
-      // Handle edit functionality
-      console.log('Editing:', item);
-    });
+//     editButton.addEventListener('click', () => {
+//       // Handle edit functionality
+//       console.log('Editing:', item);
+//     });
 
-    deleteButton.addEventListener('click', () => {
-      // Handle delete functionality
-      console.log('Deleting:', item);
-    });
+//     deleteButton.addEventListener('click', () => {
+//       // Handle delete functionality
+//       console.log('Deleting:', item);
+//     });
 
-    div.appendChild(editButton);
-    div.appendChild(deleteButton);
-    dataContainer.appendChild(div);
-  });
-}
+//     div.appendChild(editButton);
+//     div.appendChild(deleteButton);
+//     dataContainer.appendChild(div);
+//   });
+// }
 
 
